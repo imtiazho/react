642cc4619f5771c9a5a2e2266319a1318f82b393
Imtiaz <axubair9@gmail.com>
2026-02-10 07:15:43 +0600

    Topic collection about ES6

---
+++ 1 Milestone 6/4/main.js
@@ -0,0 +1,13 @@
+// How does Internet work, HTTP Vs HTTPs and What is an API
+
+// Intro to JSON, JSON Structure, parse, stringify
+
+// JSON placeholder, GET data, display data on UI
+
+// GET, POST, PATCH, DELETE, CRUD, GET Vs POST
+
+// What is rest api method
+
+// Debug API, Network tab, Status code, headers, bad API, http status code
+
+// async await
