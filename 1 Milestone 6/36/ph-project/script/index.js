87fd893d9b2269b12d38f46bf7b6199a357d3ec3
Imtiaz <axubair9@gmail.com>
2026-02-20 12:52:13 +0600

    Load videos data

--- 1 Milestone 6/36/ph-project/script/index.js
+++ 1 Milestone 6/36/ph-project/script/index.js
@@ -9,6 +9,12 @@ function loadCategories() {
     .then((data) => disPlayCategories(data.categories));
 }
 
+function loadVideoes() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 function disPlayCategories(categories) {
   const categoryContainer = document.getElementById("category-container");
 
@@ -22,4 +28,31 @@ function disPlayCategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videContainer");
+
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+            <figure>
+                <img
+                src="${video.thumbnail}"
+                alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${video.title}</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    videoContainer.append(videoCard);
+  });
+};
+
 loadCategories();
+loadVideoes();
