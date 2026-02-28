41f5cb50a269ad2899363c920e89264ea7d2a3ab
imtiazho <axubair9@gmail.com>
2026-03-01 00:40:34 +0600

    Diff type of form

--- 3 Milestone 8/react-core-3/src/--Get_form_data.js
+++ 3 Milestone 8/react-core-3/src/--Get_form_data.js
@@ -3,5 +3,5 @@
  * use form action and formData in the action handler formData.get("email")
  * Controlled component. One per each filed. Use state on change of the field. use to dynamic handle error
  * handle all controlled filed on one state object
- * Uncontrolled 
+ * Uncontrolled file
 */
