a701da82fc41f9c2f8d53f0fc9137f9dfd5170e2
imtiazho <axubair9@gmail.com>
2026-03-04 02:45:27 +0600

    Make details page

--- 3 Milestone 8/47 Project/boi-poka-project/src/Components/BookDetails/BookDetails.jsx
+++ 3 Milestone 8/47 Project/boi-poka-project/src/Components/BookDetails/BookDetails.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router";
+import { useParams } from "react-router";
 
 const BookDetails = () => {
-    
-    return (
-        <div>
-            BookDetails
-        </div>
-    );
+  const { id } = useParams();
+  const book_id = parseInt(id);
+  const data = useLoaderData();
+
+  const targetedData = data.find(book => book.bookId === book_id);
+  const {image, bookName} = targetedData;
+
+  return <div>
+    <img src={image} alt="" />
+    <h5>{bookName}</h5>
+
+    <button className="btn btn-accent m-2">Mark as Read</button>
+    <button className="btn btn-accent m-2">Wish List</button>
+  </div>;
 };
 
 export default BookDetails;
