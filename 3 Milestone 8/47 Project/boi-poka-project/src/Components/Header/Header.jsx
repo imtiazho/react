0a0063ad6bb23524ecb36013f98c499764e2cd81
imtiazho <axubair9@gmail.com>
2026-03-03 16:00:37 +0600

    useEff data fetching

--- 3 Milestone 8/47 Project/boi-poka-project/src/Components/Header/Header.jsx
+++ 3 Milestone 8/47 Project/boi-poka-project/src/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Header = () => {
+    const links = 
+    <>
+    <li className="m-2">Home</li>
+    <li className="m-2">About</li>
+    </>
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -27,26 +32,14 @@ const Header = () => {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              
-              <li>
-                <a>Item 2</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Boi Poka</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            
-            <li>
-              <a>Item 2</a>
-            </li>
+            {links}
           </ul>
         </div>
         <div className="navbar-end">
