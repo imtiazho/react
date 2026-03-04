5de09b40811b98a511ad6a1a2aba161d910599a9
imtiazho <axubair9@gmail.com>
2026-03-04 16:07:27 +0600

    add react helmet and favicon

--- 3 Milestone 8/47 Project/boi-poka-project/src/Components/Pages/Home/Home.jsx
+++ 3 Milestone 8/47 Project/boi-poka-project/src/Components/Pages/Home/Home.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Banner from '../../Banner/Banner';
-import Books from '../Books/Books';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import Banner from "../../Banner/Banner";
+import Books from "../Books/Books";
+import { useLoaderData } from "react-router";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    const data = useLoaderData();
-    // console.log(data);
+  const data = useLoaderData();
+  // console.log(data);
 
-    return (
-        <div>
-            <Banner></Banner>
-            <Books data={data}></Books>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Boi poka | home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Books data={data}></Books>
+    </div>
+  );
 };
 
 export default Home;
