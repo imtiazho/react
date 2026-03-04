b5f78dd2a11cd823190b450a67a97a2f5aea8d66
imtiazho <axubair9@gmail.com>
2026-03-04 15:35:50 +0600

    add react sweet toast

--- 3 Milestone 8/47 Project/boi-poka-project/src/Components/BookDetails/BookDetails.jsx
+++ 3 Milestone 8/47 Project/boi-poka-project/src/Components/BookDetails/BookDetails.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { useLoaderData } from "react-router";
 import { useParams } from "react-router";
 import { addToStoredDB } from "../../addToDB";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const BookDetails = () => {
   const { id } = useParams();
   const book_id = parseInt(id);
   const data = useLoaderData();
 
-  const targetedData = data.find(book => book.bookId === book_id);
-  const {image, bookName} = targetedData;
+  const targetedData = data.find((book) => book.bookId === book_id);
+  const { image, bookName, totalPages } = targetedData;
 
   const handleMarkAsRead = (id) => {
     addToStoredDB(id);
@@ -20,16 +24,31 @@ const BookDetails = () => {
     // Array or like a collection
 
     // If book already exist the a alert
+    MySwal.fire({
+      title: <p>Hello World</p>,
+      didOpen: () => {
+        // `MySwal` is a subclass of `Swal` with all the same instance & static methods
+        MySwal.showLoading();
+      },
+    }).then(() => {
+      return MySwal.fire(<p>Shorthand works too</p>);
+    });
+  };
 
-  }
-
-  return <div>
-    <img src={image} alt="" />
-    <h5>{bookName}</h5>
-
-    <button onClick={() =>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
-    <button className="btn btn-accent m-2">Wish List</button>
-  </div>;
+  return (
+    <div>
+      <img src={image} alt="" />
+      <h5>{bookName}</h5>
+      <p>{totalPages}</p>
+      <button
+        onClick={() => handleMarkAsRead(id)}
+        className="btn btn-accent m-2"
+      >
+        Mark as Read
+      </button>
+      <button className="btn btn-accent m-2">Wish List</button>
+    </div>
+  );
 };
 
 export default BookDetails;
--- 3 Milestone 8/47 Project/boi-poka-project/src/Components/Pages/ReadList/ReadList.jsx
+++ 3 Milestone 8/47 Project/boi-poka-project/src/Components/Pages/ReadList/ReadList.jsx
@@ -5,18 +5,49 @@ import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../../addToDB";
 
 const ReadList = () => {
-    const [readList, setReadList] = useState([]);
+  const [readList, setReadList] = useState([]);
   const data = useLoaderData();
+  const [sort, setSort] = useState("");
 
   useEffect(() => {
     const storedBookData = getStoredBook();
     const convertStoredBookData = storedBookData.map((id) => parseInt(id));
-    const myReadList = data.filter((book) => convertStoredBookData.includes(book.bookId));
+    const myReadList = data.filter((book) =>
+      convertStoredBookData.includes(book.bookId),
+    );
     setReadList(myReadList);
   }, [data]);
-  console.log(readList);
+
+  const handleSort = (type) => {
+    setSort(type);
+
+    if (type == "pages") {
+      const sortedBypages = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages,
+      );
+      setReadList(sortedBypages);
+      // console.log(sortedBypages);
+    }
+    if (type == "rating") {
+      const sortedByRating = [...readList].sort((a, b) => b.rating - a.rating);
+      setReadList(sortedByRating);
+      console.log(sortedByRating);
+    }
+  };
+  // console.log(readList);
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by: {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>Pages Num</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("rating")}>Rating</a>
+          </li>
+        </ul>
+      </details>
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
@@ -24,7 +55,16 @@ const ReadList = () => {
         </TabList>
 
         <TabPanel>
-          <h2>Book i read  {readList.length}</h2>
+          <h2>Book i read {readList.length}</h2>
+          <div className="flex flex-col gap-4">
+            {readList.map((eachBook) => (
+              <div>
+                <h2>{eachBook.bookName}</h2>
+                <p>{eachBook.totalPages}</p>
+                <img className="w-50" src={eachBook.image} alt="" />
+              </div>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
           <h2>My wishes</h2>
